80c04838caa4d743c78df40448411c4ac9e5ec3b
HONG ZHOU <holly_zhou@yahoo.com>
2016-09-22 19:48:02 -0700

    beginning

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = React.createClass({
+  render() {
+    return (
+      <div className="container">
+        <h1 className="text-center"></h1>
+      </div>
+    )
+  }
+});
+
+export default App;
